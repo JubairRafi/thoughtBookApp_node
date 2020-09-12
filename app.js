08ee36bfa4cac95d1e33bf5d139fe916a5498e80
jobairRafi96 <jubairrafi96960@gmail.com>
2020-09-12 14:51:55 -0700

    connect mondodb database

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express")
 const dotenv = require("dotenv")
+const connectDB = require("./config/db")
 
 
 //loading config file
 
 dotenv.config({path:"./config/config.env"})
+
+connectDB()
 
 const app = express()
 
