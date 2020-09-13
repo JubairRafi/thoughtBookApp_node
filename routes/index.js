1f4b33b1af3785f512ba071d6ef3172bef64a07b
jobairRafi96 <jubairrafi96960@gmail.com>
2020-09-13 08:21:54 -0700

    auth middleware

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,19 @@
 const express = require("express")
 const router = express.Router()
 
+const {ensureAuth,ensureGuest} = require("../middleware/auth")
+
 //description: login/landing page
 //route : GET /
 
-router.get("/",(req,res)=>{
+router.get("/", ensureGuest, (req,res)=>{
   res.render("login",{layout: "./layouts/login"})
 })
 
 //description: dashboard
 //route : GET / dashboard
 
-router.get("/dashboard",(req,res)=>{
+router.get("/dashboard", ensureAuth, (req,res)=>{
   res.render("dashboard")
 })
 
