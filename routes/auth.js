c1a7702e6f82883dd5bd1477b48aa7be992c05c5
jobairRafi96 <jubairrafi96960@gmail.com>
2020-09-13 02:33:28 -0700

    login with google done

--- routes/auth.js
+++ routes/auth.js
@@ -1,20 +1,21 @@
 const express = require("express")
+const passport = require("passport")
 const router = express.Router()
 
 //description: Auth with google
 //route : GET /auth/google
 
-router.get('/google',
-  passport.authenticate('google', { scope: ['profile'] }))
+router.get("/google",
+  passport.authenticate("google", { scope: ["profile"] }))
 
 //description: gogole auth callback
 //route : GET / auth/google/callback
 
-router.get('/auth/google/callback',
-  passport.authenticate('google', { failureRedirect: '/' }),
+router.get("/google/callback",
+  passport.authenticate("google", { failureRedirect: "/" }),
   (req, res)=>{
     // Successful authentication, redirect home.
-    res.redirect('/dashboard');
+    res.redirect("/dashboard");
   });
 
 module.exports = router
