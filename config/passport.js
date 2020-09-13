6f1c9d320ee3867cba3abecc71d64ea05e2a01d1
jobairRafi96 <jubairrafi96960@gmail.com>
2020-09-13 01:44:37 -0700

    user model create and passport google strategy function

--- config/passport.js
+++ config/passport.js
@@ -1,1 +1,16 @@
 const googleStrategy = require("passport-google-oauth20").Strategy
+const mongoose = require("mongoose")
+const User = require("../models/User")
+
+module.exports = function(passport){
+      passport.use(new googleStrategy({
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        calbackURL: "/auth/google/callback"
+      },
+      async(accessToken, refreshToken, profile, done)=>{
+        console.log(profile);
+      }
+
+  ))
+}
